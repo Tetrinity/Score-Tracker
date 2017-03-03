5671e9aeee53ae715d9b3f854d693333f1e7315b
Tetrinity <zephyrsg@hotmail.co.uk>
2017-03-03 17:56:39 +0000

    remove unused import

--- app/src/main/java/com/tetrinity/scoretracker/game/Game.java
+++ app/src/main/java/com/tetrinity/scoretracker/game/Game.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Set;
 
 public class Game implements Serializable {
 
