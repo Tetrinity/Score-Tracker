6d155574ce48deb284788d8d35f8adfc67d85210
Tetrinity <zephyrsg@hotmail.co.uk>
2017-03-25 20:50:24 +0000

    Added unit test for increasing player count.

--- app/src/test/java/com/tetrinity/scoretracker/game/GameUnitTest.java
+++ app/src/test/java/com/tetrinity/scoretracker/game/GameUnitTest.java
@@ -32,11 +32,13 @@ public class GameUnitTest {
         game.setMoves(moves);
 
         Assert.assertEquals("Expected setup to have 2 move rows", 2, game.getMoveRowCount());
+        Assert.assertEquals("Expected setup to have 3 players", 3, game.getPlayerCount());
         Assert.assertEquals("Expected setup to have a total of 6 moves", 6, game.getTotalMoveCount());
 
         game.addMoveRow();
 
         Assert.assertEquals("Expected to have 3 move rows after adding a move row", 3, game.getMoveRowCount());
+        Assert.assertEquals("Expected to have 3 players after adding a move row", 3, game.getPlayerCount());
         Assert.assertEquals("Expected to have a total of 9 moves after adding a move row", 9, game.getTotalMoveCount());
 
         List<List<Move>> actualMoves = game.getMoves();
@@ -45,6 +47,31 @@ public class GameUnitTest {
             Assert.assertEquals("Expected newly created move to default to 0", 0, lastMove.score);
             Assert.assertEquals("Expected newly created move to have no word", "", lastMove.word);
         }
+    }
+
+    @Test
+    public void test_setPlayerCount_Increase(){
+        Game game = new Game();
+
+        ArrayList<String> players = new ArrayList<String>();
+        players.add("Player 1"); players.add("Player 2");
+        game.setPlayerNames(players);
+
+        ArrayList<List<Move>> moves = new ArrayList<>();
+        moves.add(createMoveList(new Move(1), new Move(2)));
+        moves.add(createMoveList(new Move(3), new Move(4)));
+        game.setMoves(moves);
+
+        Assert.assertEquals("Expected setup to have 2 move rows", 2, game.getMoveRowCount());
+        Assert.assertEquals("Expected setup to have 2 players", 2, game.getPlayerCount());
+        Assert.assertEquals("Expected setup to have a total of 4 moves", 4, game.getTotalMoveCount());
+
+        game.setPlayerCount(3);
+
+        Assert.assertEquals("Expected to have 2 move rows after increasing player count", 2, game.getMoveRowCount());
+        Assert.assertEquals("Expected to have 3 players after increasing player count", 3, game.getPlayerCount());
+        Assert.assertEquals("Expected to have a total of 6 moves after increasing player count", 6, game.getTotalMoveCount());
+
     }
 
     private List<Move> createMoveList(Move... rawMoveArray){
